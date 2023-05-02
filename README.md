6138bb81647168bd5954b32d8cf7a94459281c21
Ivan <ivan.yarovoy1@yandex.ry>
2023-05-03 01:39:31 +0300

    Changed data storage to linked list, removed limitation on entry size and added mutexes to avoid race condition.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-A linux kernel module that creates a readable and writable file /proc/test_task. You can test it with cat and echo, for example "echo '1' > /proc/test_task" "cat /proc/test_task".
+A linux kernel module that creates a readable and writable file /proc/test_task.
+You can test it with cat and echo, for example "echo '1' > /proc/test_task" "cat /proc/test_task".
+For testing with large data, type "cat large_file.txt > /proc/test_task", then "diff large_file.txt /proc/test_task", this command will display mistakes in copying if any exists. 
 
 How to use:
 	1. Enter "make" in root directory of this project to compile.
--- proc_temp_mem.c
+++ proc_temp_mem.c
@@ -1,8 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
-#include <linux/string.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
@@ -10,50 +10,96 @@
 #endif
 
 #define PROCFS_NAME "test_task"
-#define MAX_SINGLE_ENTRY 512
+
+struct node {
+  char *data;
+  size_t length;
+  struct node *next;
+};
+
+struct node *head = NULL;
+
+size_t overall_length = 0;
+
+struct mutex rw_mutex;
 
 static struct proc_dir_entry *our_proc_file;
 
-static char *procfs_buffer = NULL;
-
-static unsigned long procfs_buffer_size = 0;
-
 static ssize_t procfile_read(struct file *filePointer, char __user *buffer,
                              size_t buffer_length, loff_t *offset) {
-  int len = strlen(procfs_buffer);
-  ssize_t ret = len;
-  if (*offset >= len || copy_to_user(buffer, procfs_buffer, len)) {
-    pr_info("copy_to_user failed\n");
-    ret = 0;
-  } else {
-    pr_info("procfile read %s\n", filePointer->f_path.dentry->d_name.name);
-    *offset += len;
+  mutex_lock(&rw_mutex);
+  if (*offset >= overall_length) {
+    mutex_unlock(&rw_mutex);
+    return 0;
   }
-  return ret;
+  struct node *curr_element = head;
+  size_t copied_symbols = 0;
+  size_t skipped_symbols = 0;
+  while (skipped_symbols + curr_element->length < *offset) {
+    skipped_symbols += curr_element->length;
+    curr_element = curr_element->next;
+  }
+  size_t position = *offset - skipped_symbols;
+  while (copied_symbols < buffer_length) {
+    if (*offset >= overall_length)
+      break;
+    if (buffer_length - copied_symbols >= curr_element->length - position) {
+      if (copy_to_user(buffer + copied_symbols, curr_element->data + position,
+                       curr_element->length - position)) {
+        pr_alert("Error: copy_to_user failed\n");
+        mutex_unlock(&rw_mutex);
+        return -EFAULT;
+      }
+      *offset += curr_element->length - position;
+      copied_symbols += curr_element->length - position;
+      position = 0;
+      if (curr_element->next == NULL)
+        break;
+      curr_element = curr_element->next;
+    } else {
+      if (copy_to_user(buffer + copied_symbols, curr_element->data + position,
+                       buffer_length - copied_symbols)) {
+        pr_alert("Error: copy_to_user failed\n");
+        mutex_unlock(&rw_mutex);
+        return -EFAULT;
+      }
+      *offset += (buffer_length - copied_symbols);
+      copied_symbols += (buffer_length - copied_symbols);
+    }
+  }
+  mutex_unlock(&rw_mutex);
+  return copied_symbols;
 }
 
 static ssize_t procfile_write(struct file *file, const char __user *buff,
                               size_t len, loff_t *off) {
-  char data_to_write[MAX_SINGLE_ENTRY];
-  unsigned int entry_len = len;
-  if (entry_len > MAX_SINGLE_ENTRY)
-    entry_len = MAX_SINGLE_ENTRY;
-
-  if (copy_from_user(data_to_write, buff, entry_len))
-    return -EFAULT;
-
-  data_to_write[(entry_len < MAX_SINGLE_ENTRY) ? entry_len
-                                               : MAX_SINGLE_ENTRY - 1] = '\0';
-  procfs_buffer_size = strlen(procfs_buffer) + strlen(data_to_write);
-  procfs_buffer = (char *)krealloc(
-      procfs_buffer, procfs_buffer_size * sizeof(char), GFP_KERNEL);
-  if (NULL == procfs_buffer) {
-    pr_alert("Error: could not allocate memory");
+  struct node *new_element =
+      (struct node *)kmalloc(sizeof(struct node), GFP_KERNEL);
+  if (NULL == new_element) {
+    pr_alert("Error: could not allocate memory\n");
     return -ENOMEM;
   }
-  strcat(procfs_buffer, data_to_write);
-  pr_info("procfile write %s\n", procfs_buffer);
-  return procfs_buffer_size;
+  new_element->data = (char *)kmalloc(len * sizeof(char), GFP_KERNEL);
+  if (NULL == new_element->data) {
+    pr_alert("Error: could not allocate memory\n");
+    return -ENOMEM;
+  }
+  new_element->length = len;
+  new_element->next = NULL;
+  if (copy_from_user(new_element->data, buff, len))
+    return -EFAULT;
+  mutex_lock(&rw_mutex);
+  overall_length += len;
+  if (head == NULL)
+    head = new_element;
+  else {
+    struct node *element = head;
+    while (element->next != NULL)
+      element = element->next;
+    element->next = new_element;
+  }
+  mutex_unlock(&rw_mutex);
+  return new_element->length;
 }
 
 #ifdef HAVE_PROC_OPS
@@ -69,27 +115,32 @@ static const struct file_operations proc_file_fops = {
 #endif
 
 static int __init proc_temp_mem_init(void) {
+  mutex_init(&rw_mutex);
   our_proc_file = proc_create(PROCFS_NAME, 0666, NULL, &proc_file_fops);
   if (NULL == our_proc_file) {
     proc_remove(our_proc_file);
     pr_alert("Error:Could not initialize /proc/%s\n", PROCFS_NAME);
     return -ENOMEM;
   }
-  procfs_buffer = (char *)kmalloc(sizeof(char), GFP_KERNEL);
-  if (NULL == procfs_buffer) {
-    proc_remove(our_proc_file);
-    pr_alert("Error:could not allocate memory");
-    return -ENOMEM;
-  }
-  procfs_buffer[0] = '\0';
   pr_info("/proc/%s created\n", PROCFS_NAME);
   return 0;
 }
 
 static void __exit proc_temp_mem_exit(void) {
   proc_remove(our_proc_file);
-  kfree(procfs_buffer);
   pr_info("/proc/%s removed\n", PROCFS_NAME);
+  if (head == NULL)
+    return;
+  struct node *curr_element = head;
+  struct node *prev_element = head;
+  while (curr_element->next != NULL) {
+    curr_element = curr_element->next;
+    kfree(prev_element->data);
+    kfree(prev_element);
+    prev_element = curr_element;
+  }
+  kfree(curr_element->data);
+  kfree(curr_element);
 }
 
 module_init(proc_temp_mem_init);
